bfadfbfa542867253a5dda03cc8fbf90b63a562e
Abhi <162410103+Abhi0065@users.noreply.github.com>
2025-03-31 19:14:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ async function sendUrl(req, res) {
       express.static(path.join(__dirname, 'node_modules/codemirror')),
     );
     app.use(route, express.static(path.join(__dirname, 'editor')));
-    const url = `https://compiler-cpp-production.up.railway.app/${route}`;
+    const url = `https://compiler-cpp-production.up.railway.app${route}`;
     res.status(200).json({ message: true, url });
   } catch (error) {
     console.error('Fetch error:', error);
