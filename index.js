49e39a8b5976f723af91a406107299b1872896a7
Abhi <162410103+Abhi0065@users.noreply.github.com>
2025-03-31 16:15:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const wss = new WebSocketServer({ server });
 var currentCode;
 var filename;
 
-const mainDomain = 'https://compiler-cpp06.onrender.com';
+const mainDomain = 'https://compiler-cpp.onrender.com';
 const requestedDomain = 'https://file-manager-cpp06.onrender.com';
 
 app.use(express.static(path.join(__dirname, 'public')));
