62703e268eed2b0087f47129a372da7fc9d13f73
Abhi <162410103+Abhi0065@users.noreply.github.com>
2025-06-06 22:07:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
 
-const mainDomain = 'https://compiler-cpp-production.up.railway.app';
-const requestedDomain = 'https://file-manager-cpp-production.up.railway.app';
+const mainDomain = 'https://compiler-cpp.onrender.com';
+const requestedDomain = 'https://file-manager-cpp.onrender.com;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
